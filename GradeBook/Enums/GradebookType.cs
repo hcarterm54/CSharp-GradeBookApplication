29f1080bb298403c1aa14d0d55b891825199919d
Carter Mitchell <carter-mitchell@outlook.com>
2019-01-09 12:39:24 -0600

    Second commit after initial push failed

---
+++ GradeBook/Enums/GradebookType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.Enums
+{
+    public enum GradebookType
+    {
+        Standard,
+        Ranked,
+        ESNU,
+        OneToFour,
+        SixPoint
+    }
+}
